9e814e086c6a5d05d37aa731185c48e85b03ef2f
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-07 21:51:00 +0700

    Added Type Data "Undefined"

---
+++ Modul1-FundamentalJavaScript/TipeData.js
@@ -0,0 +1,6 @@
+let x;
+console.log(typeof(x));
+
+/* Output: Undefined 
+Fungsi typeof() digunakan untuk memastikan tipe data pada variabel dengan mengembalikan tipe data tersebut dalam bentuk teks.
+*/
