38122b21308f4ea0d05b815b2b5243b9ed60e44d
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-07 21:53:15 +0700

    Added Tipe Data "Number"

--- Modul1-FundamentalJavaScript/TipeData.js
+++ Modul1-FundamentalJavaScript/TipeData.js
@@ -4,3 +4,13 @@ console.log(typeof(x));
 /* Output: Undefined 
 Fungsi typeof() digunakan untuk memastikan tipe data pada variabel dengan mengembalikan tipe data tersebut dalam bentuk teks.
 */
+
+let y = 10;
+console.log(typeof(y))
+
+/* Output Number */
+
+let z = 17.25;
+console.log(typeof(z))
+
+/* Output Number */
