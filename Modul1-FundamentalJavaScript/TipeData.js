dddee7633512f40b85c934f9b35fd4618e555b88
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-07 21:59:32 +0700

    Added Tipe Data "Operator Logika"

--- Modul1-FundamentalJavaScript/TipeData.js
+++ Modul1-FundamentalJavaScript/TipeData.js
@@ -1,16 +1,28 @@
 let x;
 console.log(typeof(x));
-
 /* Output: Undefined 
 Fungsi typeof() digunakan untuk memastikan tipe data pada variabel dengan mengembalikan tipe data tersebut dalam bentuk teks.
 */
 
 let y = 10;
 console.log(typeof(y))
-
 /* Output Number */
 
 let z = 17.25;
 console.log(typeof(z))
-
 /* Output Number */
+
+let a = 12;
+let b = 9;
+console.log (a + b)
+console.log (a - b)
+console.log (a * b)
+console.log (a / b)
+console.log (a % b)
+/* output:
+21
+3
+108
+1.3333333333333333
+3
+*/
