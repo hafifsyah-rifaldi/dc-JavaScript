2a958a829b1e8e36bb40c5d9e5e020c743e1c95d
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-07 22:08:49 +0700

    Added "bigInt Operasi Aritmatika"

--- Modul1-FundamentalJavaScript/TipeData.js
+++ Modul1-FundamentalJavaScript/TipeData.js
@@ -43,8 +43,20 @@ const myInt = 1234567890123456789012345678901234567890;
 
 console.log(bigNumber);
 console.log(myInt);
+console.log(5n + 2n)
+console.log(5n - 2n)
+console.log(5n * 2n)
+console.log(5n / 2n)
+console.log(5n % 2n)
 
 /* output 
 1234567890123456789012345678901234567890n
 1.2345678901234568e+39
+*/
+/* output
+7n
+3n
+10n
+2n; Bukan 2.5n
+1n
 */
