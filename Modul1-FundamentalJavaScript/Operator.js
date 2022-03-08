5d071fa62eafafd1f9d99e7fd1f1b8bd3ac906b6
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-08 20:39:11 +0700

    Added "Perbedaan Sama dan Identik"

--- Modul1-FundamentalJavaScript/Operator.js
+++ Modul1-FundamentalJavaScript/Operator.js
@@ -9,7 +9,7 @@ x %= y; //artinya -> x = x % y;
 console.log(x);
 
 /* output
-15
+0
 */
 
 let a = 10;
@@ -18,6 +18,16 @@ let b = 12;
 console.log(a < b);
 console.log(a > b);
 
+/* output
+true
+false
+*/
+
+const aString = '10';
+const aNumber = 10
+
+console.log(aString == aNumber) // true, karena nilainya sama-sama 10
+console.log(aString === aNumber) // false, karena walaupun nilainya sama, tetapi tipe datanya berbeda
 /* output
 true
 false
