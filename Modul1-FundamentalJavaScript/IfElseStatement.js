f62ce1de9dae90123f4711d1935cef575bc77a47
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-08 20:53:42 +0700

    Added Conditional Expressions

--- Modul1-FundamentalJavaScript/IfElseStatement.js
+++ Modul1-FundamentalJavaScript/IfElseStatement.js
@@ -38,3 +38,13 @@ console.log(greeting);
 /* Output
 Bonjour!
 */
+
+// condition ? true expression : false expression
+
+const isMember = false;
+const discount = isMember ? 0.1 : 0;
+console.log(`Anda mendapatkan diskon sebesar ${discount * 100}%`)
+
+/* output
+Anda mendapatkan diskon sebesar 0%
+ */
