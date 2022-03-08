405c3d2f3c4786ad67d5da0545937b29dac92317
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-08 20:35:05 +0700

    Added Operator

--- Modul1-FundamentalJavaScript/Operator.js
+++ Modul1-FundamentalJavaScript/Operator.js
@@ -1,8 +1,11 @@
 let x = 10;
 let y = 5;
 
-x += y;
-
+x += y; //artinya -> x = x + y;
+x -= y; //artinya -> x = x - y;
+x *= y; //artinya -> x = x * y;
+x /= y; //artinya -> x = x / y;
+x %= y; //artinya -> x = x % y;
 console.log(x);
 
 /* output
