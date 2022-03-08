6e140b5969a3ef015fdd186f95e01ca954161eac
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-08 20:47:25 +0700

    Added If Else statement

---
+++ Modul1-FundamentalJavaScript/IfElseStatement.js
@@ -0,0 +1,12 @@
+const isRaining = true;
+
+console.log("Persiapan sebelum berangkat kegiatan.");
+if (isRaining) {
+    console.log("Hari ini hujan. Bawa payung.");
+}
+console.log("Berangkat kegiatan");
+/* output:
+Persiapan sebelum berangkat kegiatan.
+Hari ini hujan. Bawa payung.
+Berangkat kegiatan.
+ */
