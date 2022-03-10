3e8e114ca99425bde56130d581e90e235b3d6671
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 21:46:35 +0700

    Added Set fungsi Add()

--- Modul2-StrukturData/Set.js
+++ Modul2-StrukturData/Set.js
@@ -1,6 +1,12 @@
 const numberSet = new Set([1, 4, 6, 4, 1]);
+
+//! Menambahkan fungsi add()
+numberSet.add(5);
+numberSet.add(10);
+numberSet.add(6);
+
 console.log(numberSet);
 
 /* Output
-Set(3) { 1, 4, 6}
+Set(5) { 1, 4, 6, 5, 10}
 */
