d91e25f2a46a55ccaa3c2346f041b8d9df2ea4b8
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 21:25:27 +0700

    Added syntax Destructuring Array

--- Modul2-StrukturData/DestructuringArray.js
+++ Modul2-StrukturData/DestructuringArray.js
@@ -25,3 +25,49 @@ let herFood = "Mie";
 
 console.log(myFood);
 console.log(herFood);
+
+
+// ! ALgoritma Sorting tanpa Destructuring Array
+var a = 1;
+var b = 2;
+var temp;
+ 
+console.log("Sebelum swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
+ 
+temp = a;
+a = b;
+b = temp;
+ 
+console.log("Setelah swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
+/* output
+Sebelum swap
+Nilai a: 1
+Nilai b: 2
+Setelah swap
+Nilai a: 2
+Nilai b: 1
+*/
+
+// ! Algoritma sorting dengan Array Destructuring Assignment
+console.log("Sebelum swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
+ 
+[a, b] = [b, a]
+ 
+console.log("Setelah swap");
+console.log("Nilai a: " + a);
+console.log("Nilai b: " + b);
+ 
+/* output
+Sebelum swap
+Nilai a: 1
+Nilai b: 2
+Setelah swap
+Nilai a: 2
+Nilai b: 1
+*/
