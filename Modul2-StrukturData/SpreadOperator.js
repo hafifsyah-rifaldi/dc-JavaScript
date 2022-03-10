8718b2e37a4ca327df872605e7b045a327a6b16b
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 20:45:44 +0700

    Added Spread Operator

---
+++ Modul2-StrukturData/SpreadOperator.js
@@ -0,0 +1,10 @@
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+const others = ["Cake", "Pie", "Donut"];
+
+const allFavorites = [favorites, others];
+console.log(...favorites);
+console.log(allFavorites);
+
+/* output
+[ 'Seafood', 'Salad', 'Nugget', 'Soup' ]
+*/
