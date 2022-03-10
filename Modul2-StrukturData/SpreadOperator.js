b53f3c87a87827746e143cb418364fb421b343aa
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 20:48:22 +0700

    Added Spread Operator Dua Array

--- Modul2-StrukturData/SpreadOperator.js
+++ Modul2-StrukturData/SpreadOperator.js
@@ -1,8 +1,9 @@
 const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
 const others = ["Cake", "Pie", "Donut"];
-
-const allFavorites = [favorites, others];
 console.log(...favorites);
+
+const allFavorites = [...favorites, ...others];
+
 console.log(allFavorites);
 
 /* output
