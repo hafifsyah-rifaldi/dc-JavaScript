fb5dcbfcb0a4c6f3ed2870299cccb723963da130
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 21:36:30 +0700

    Added Map

---
+++ Modul2-StrukturData/Map.js
@@ -0,0 +1,28 @@
+const myMap = new Map ([
+    ['1', 'a String key'],
+    [1, 'a number key'],
+    [true, true]
+]);
+
+console.log(myMap);
+/* output
+Map(3) { '1' => 'a String key', 1 => 'a number key', true => true }
+*/
+
+// ! Ketika sudah membuat objek Map, kita bisa mendapatkan nilainya berdasarkan key tertentu dengan metode get(). Lalu, untuk menambahkan pasangan key-value baru gunakan metode set().
+const capital = new Map([
+    ["Jakarta", "Indonesia"],
+    ["London", "England"],
+    ["Tokyo", "Japan"]
+]);
+console.log(capital.size);
+console.log(capital.get("London"));
+capital.set("New Delhi", "India");
+console.log(capital.size);
+console.log(capital.get("New Delhi"));
+/* output
+3
+England
+4
+India
+ */
