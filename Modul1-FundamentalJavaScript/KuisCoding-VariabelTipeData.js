5a94b46ba54df3fe79662203b16b1b9c5f285fcf
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 14:43:01 +0700

    Added KuisCoding

---
+++ Modul1-FundamentalJavaScript/KuisCoding-VariabelTipeData.js
@@ -0,0 +1,23 @@
+let firstName = "Hafifsyah";
+let lastName = "Rifaldi";
+const age = 22;
+let isMarried = false;
+console.log(firstName,lastName,age,isMarried);
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+
+module.exports = {
+  firstName, lastName, age, isMarried,
+};
