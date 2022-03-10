a1a067be8873cd4ed8b3c4be9c84778215aca68a
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 21:48:34 +0700

    Added Set fungsi delete()

--- Modul2-StrukturData/Set.js
+++ Modul2-StrukturData/Set.js
@@ -4,9 +4,11 @@ const numberSet = new Set([1, 4, 6, 4, 1]);
 numberSet.add(5);
 numberSet.add(10);
 numberSet.add(6);
+//! Menambah fungsi delete()
+numberSet.delete(4);
 
 console.log(numberSet);
 
-/* Output
-Set(5) { 1, 4, 6, 5, 10}
+/* output
+Set(4) { 1, 6, 5, 10 }
 */
