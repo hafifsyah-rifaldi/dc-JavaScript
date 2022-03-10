cea8f3a6172ed88220f69e5627325df90ba8169f
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 19:50:20 +0700

    Added properti Object

--- Modul2-StrukturData/Object.js
+++ Modul2-StrukturData/Object.js
@@ -3,10 +3,13 @@ const user = {
     lastName: "Skywalker",
     age: 19,
     isJedi: true,
+    "home world": "Tattooine" /* user[“home world”];
+    Untuk mengakses key yang memiliki spasi atau karakter khusus lainnya maka kita perlu menggunakan bracket seperti di atas.*/
 };
 
 console.log(`halo, nama saya ${user.firstName} ${user.lastName}`);
 console.log(`Umur saya ${user.age} tahun`);
+console.log(`Saya berasal dari ${user["home world"]}`);
 
 /* Output 
 Halo, nama saya Luke Skywalker
