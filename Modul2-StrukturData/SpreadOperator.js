bcf39f09f21dcd3ad7376c00a465ce6c0efb2498
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 20:50:55 +0700

    Added Spread Operator (Object Literals)

--- Modul2-StrukturData/SpreadOperator.js
+++ Modul2-StrukturData/SpreadOperator.js
@@ -9,3 +9,14 @@ console.log(allFavorites);
 /* output
 [ 'Seafood', 'Salad', 'Nugget', 'Soup' ]
 */
+
+
+//Spread operator untuk object literals, menggabungkan object dengan kode yang lebih ringkas
+const obj1 = { firstName: 'Obi-Wan', age: 30};
+const obj2 = { lastName: 'Kenobi', gender: 'M'};
+
+const newObj = { ...obj1, ...obj2};
+console.log(newObj);
+/* output
+{ firstName: 'Obi-Wan', age: 30, lastName: 'Kenobi', gender: 'M' }
+*/
