3f2b18ffed374da5af4d4c0387beda52d3f28d37
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 21:21:56 +0700

    Added Destructuring Assignment

--- Modul2-StrukturData/DestructuringArray.js
+++ Modul2-StrukturData/DestructuringArray.js
@@ -8,10 +8,20 @@ console.log(fourthFood);
 
 
 // mengambil nilai array tanpa index lainnya
-
 const [, , _3Food] = favorites;
 console.log(_3Food);
 /*
 output:
 Nugget
 */
+
+// Destructuring Assignment
+const favorit = ["Kentang", "Udang", "Nugget", "Ayam"];
+
+let myFood = "Es krim";
+let herFood = "Mie";
+
+[myFood, herFood] = favorit;
+
+console.log(myFood);
+console.log(herFood);
