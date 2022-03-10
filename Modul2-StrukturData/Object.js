3016572f6a752f3e777c8b8527fc6a3233fc5d42
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 19:55:45 +0700

    Added Object Property Assignment Operator

--- Modul2-StrukturData/Object.js
+++ Modul2-StrukturData/Object.js
@@ -14,4 +14,29 @@ console.log(`Saya berasal dari ${user["home world"]}`);
 /* Output 
 Halo, nama saya Luke Skywalker
 Umur saya 19 tahun
+Saya berasal dari Tattooine
 */
+
+
+/* Memodifikasi Object dengan mengubah nilai properti dalam object 
+menggunakan assignment operator (=)
+*/
+
+const spaceship = {
+    name: "Millenium Falcon",
+    manufacturer: "Corellian Engineering Corporation",
+    maxSpeed: 1200,
+    color: "Light gray"
+};
+
+spaceship.color = "Glossy red";
+spaceship["maxSpeed"] = 1300;
+console.log(spaceship);
+/* output
+{
+  name: 'Millenium Falcon',
+  manufacturer: 'Corellian Engineering Corporation',
+  maxSpeed: 1300,
+  color: 'Glossy red'
+}
+ */
