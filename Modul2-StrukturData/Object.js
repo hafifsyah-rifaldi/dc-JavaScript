91138e118f83bfd7b65c5ebc4b5593953e18f47a
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 19:47:03 +0700

    Added Object

---
+++ Modul2-StrukturData/Object.js
@@ -0,0 +1,14 @@
+const user = {
+    firstName: "Luke",
+    lastName: "Skywalker",
+    age: 19,
+    isJedi: true,
+};
+
+console.log(`halo, nama saya ${user.firstName} ${user.lastName}`);
+console.log(`Umur saya ${user.age} tahun`);
+
+/* Output 
+Halo, nama saya Luke Skywalker
+Umur saya 19 tahun
+*/
