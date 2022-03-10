b5dbe9b230e6f5157761123117455ec0d12241d1
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 21:15:24 +0700

    Added Destructuring Array

---
+++ Modul2-StrukturData/DestructuringArray.js
@@ -0,0 +1,7 @@
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
+
+const [firstFood, secondFood, thirdFood, fourthFood] = favorites;
+console.log(firstFood);
+console.log(secondFood);
+console.log(thirdFood);
+console.log(fourthFood);
