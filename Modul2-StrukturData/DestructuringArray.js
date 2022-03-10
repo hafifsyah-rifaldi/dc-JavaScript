4506e6cc769027f3622cdf80b1023d8fcfc7af3f
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-10 21:18:06 +0700

    Added syntax

--- Modul2-StrukturData/DestructuringArray.js
+++ Modul2-StrukturData/DestructuringArray.js
@@ -5,3 +5,13 @@ console.log(firstFood);
 console.log(secondFood);
 console.log(thirdFood);
 console.log(fourthFood);
+
+
+// mengambil nilai array tanpa index lainnya
+
+const [, , _3Food] = favorites;
+console.log(_3Food);
+/*
+output:
+Nugget
+*/
