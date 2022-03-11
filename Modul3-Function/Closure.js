1233cd1704f0520f29dd2d26f069bb05f894ec5e
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-11 15:54:47 +0700

    Added Closure

--- Modul3-Function/Closure.js
+++ Modul3-Function/Closure.js
@@ -1,6 +1,10 @@
 /* Suatu fungsi yang dapat mengakses variabel di dalam lexical scope-nya disebut dengan closure. 
 Lexical scope berarti pada sebuah fungsi bersarang, 
 fungsi yang berada di dalam memiliki akses ke variabel di lingkup induknya. */
+
+const { objToLists } = require("prelude-ls");
+
+//* Lexical Scope
 function init() {
     var name = 'Obi Wan '; // Variable lokal di dalam scope fungsi init
 
@@ -15,3 +19,21 @@ init();
 /* output
 Halo, Obi Wan
 */
+
+function init() {
+    var name = "Wan Obi";
+
+    function greet() {
+        console.log(`Halo, ${name}`);
+    }
+
+    return greet;
+}
+
+let myFunction = init();
+myFunction();
+/* Output
+halo, Wan Obi */
+/* 
+Kode di atas akan menghasilkan output yang sama. 
+Perbedaannya adalah fungsi greet() dikembalikan (return) dari outer function-nya sebelum dieksekusi. */
