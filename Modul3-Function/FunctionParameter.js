0204580f0e86c4c1fd45539b74c841dc0be34eb5
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-11 14:13:07 +0700

    Added Function Parameter

---
+++ Modul3-Function/FunctionParameter.js
@@ -0,0 +1,13 @@
+const user = {
+    id: 24,
+    displayName: 'kren',
+    fullName: 'Kylo Ren',
+};
+
+function introduce ({displayName, fullName}) {
+    console.log((`${displayName} is ${fullName}`));
+}
+introduce (user);
+/* output
+kren is Kylo Ren
+*/
