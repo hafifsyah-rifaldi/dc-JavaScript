7f1a417dc131fbd27f0d8300357bdcdaac39d828
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-11 16:40:09 +0700

    Added function power to KuisCoding-Function

--- Modul3-Function/KuisCoding-Function.js
+++ Modul3-Function/KuisCoding-Function.js
@@ -37,6 +37,14 @@ console.log(minimal(1, 4));
 console.log(minimal(3, 2));
 console.log(minimal(3, 3));
 
+
+function power(a, b){
+    return Math.pow(a, b);
+}
+
+console.log(power(7, 3));
+console.log(power(3, 3));
+console.log(power(4, 0.5));
 /**
  * Jangan hapus kode di bawah ini
  */
