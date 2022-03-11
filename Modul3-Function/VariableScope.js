03c1f083c645d13d4e45e34a70674e51c8f4859a
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-11 14:55:20 +0700

    Added Definition Function Global/Locally

--- Modul3-Function/VariableScope.js
+++ Modul3-Function/VariableScope.js
@@ -18,3 +18,29 @@ function parent() {
         const c = 'c';
     }
 }
+
+
+//* Berhati hati dalam mendefinisikan variabel dalam fungsi
+/* 
+Kita harus berhati-hati dalam mendefinisikan variabel di dalam fungsi. 
+Pasalnya, kita bisa mendapatkan hasil yang tidak diperkirakan, 
+contohnya seperti berikut: */
+function multiply(num) {
+    total = num * num;
+    return total;
+}
+
+let total = 9;
+let number = multiply(20);
+
+console.log(total);
+/* output
+400
+*/
+
+/* 
+Mungkin kita berharap nilai total akan tetap 9, 
+mengingat variabel total pada fungsi multiply seharusnya tidak akan berpengaruh untuk kode di luar dari fungsi tersebut. 
+Hal ini bisa terjadi karena pada fungsi multiply() kita tidak menetapkan variabel total sebagai cakupan lokal. 
+Kita tidak menggunakan keyword const atau let ketika mendeklarasikan variabel total pada fungsi multiply() sehingga variabel total menjadi global. */
+//! Perlu kita perhatikan, jika kita lupa menuliskan keyword let, const, atau var pada script ketika membuat sebuah variabel, maka variabel tersebut akan menjadi global.
