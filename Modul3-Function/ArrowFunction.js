18320ec29dec95162b908721bb4f046380cfb4f7
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-11 14:36:21 +0700

    Added Arrow Function

---
+++ Modul3-Function/ArrowFunction.js
@@ -0,0 +1,20 @@
+//* REGULAR FUNCTION
+// function declaration
+function sayHello(greet) {
+    console.log(`${greet}!`);
+}
+
+// function expression
+const sayName = function (name) {
+    console.log(`Nama saya ${name}`);
+}
+
+//* ARROW FUNCTION
+//function expression
+const sayHi = (greet) => {
+    console.log(`${greet}!`)
+}
+
+const sayNama = (nama) => {
+    console.log(`Nama saya ${nama}`)
+}
