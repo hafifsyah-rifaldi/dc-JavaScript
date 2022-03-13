8f010404563fb1060c2427f653b02cf09355fd31
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-13 20:03:49 +0700

    Added code Array Sort

--- Modul5-FunctionalProgramming/Reusable-Function.js
+++ Modul5-FunctionalProgramming/Reusable-Function.js
@@ -105,3 +105,14 @@ console.log(findJames);
 output
 { name: 'James', score: 88 }
 **/
+
+//* ARRAY SORT
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+// Output: [ 'Dec', 'Feb', 'Jan', 'March' ]
+
+const array1 = [1, 30, 4, 1000, 101, 121];
+array1.sort();
+console.log(array1);
+// output: [ 1, 1000, 101, 121, 30, 4 ]
