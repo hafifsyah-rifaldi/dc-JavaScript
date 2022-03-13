23c759c1d43c520468ac0d3f346917c9fea2b70d
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-13 19:49:19 +0700

    Added Code Higher-Order Function

--- Modul5-FunctionalProgramming/Konsep-FunctionalProgramming.js
+++ Modul5-FunctionalProgramming/Konsep-FunctionalProgramming.js
@@ -160,3 +160,53 @@ const countDown = start => {
 };
 
 countDown(10);
+
+
+//* HIGHER-ORDER FUNCTION
+/* JavaScript memiliki kemampuan First Class Functions, 
+karena itu fungsi pada JavaScript dapat diperlakukan layaknya sebuah data. 
+Kita bisa menyimpan function dalam variabel, 
+memberikan function sebagai parameter pada fungsi lainnya, 
+hingga mengembalikan function di dalam function. */
+
+const hello = () => {
+    console.log('Hello!')
+};
+const say = (someFunction) => {
+    someFunction();
+}
+
+const sayHello = () => {
+    return () => {
+        console.log('Hello!');
+    }
+}
+
+hello();
+say(hello);
+sayHello()();
+/** output:
+ * Hello!
+ * Hello!
+ * Hello!
+ */
+/*  Dengan mengetahui adanya Higher-Order Function, 
+Anda bisa membuat fungsi map() versi Anda sendiri! */
+
+
+/* const names = ['Harry', 'Ron', 'Jeff', 'Thomas']; */
+const arrayMap= (arr, action) => {
+    const loopTrough = (arr, action, newArray = [], index = 0) => {
+        const item = arr[index];
+        if(!item) return newArray;
+        return loopTrough(arr, action, [...newArray, action(arr[index])], index +1 );
+    }
+
+    return loopTrough(arr, action);
+}
+
+const newNames = arrayMap(names, (name) => `${name}!` );
+console.log({
+    names,
+    newNames,
+});
