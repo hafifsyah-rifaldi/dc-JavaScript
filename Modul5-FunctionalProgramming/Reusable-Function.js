c4ea3a67cb0172e9d022bfbe4201940c17594c12
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-13 20:07:21 +0700

    Added code Array Every

--- Modul5-FunctionalProgramming/Reusable-Function.js
+++ Modul5-FunctionalProgramming/Reusable-Function.js
@@ -129,3 +129,14 @@ console.log(sorting);
 output
 [ 1, 4, 30, 1000 ]
 **/
+
+//* ARRAY EVERY
+const scores = [70,85,90];
+const minimumScore = 65;
+
+const examPassed = scores.every(score => score >= minimumScore)
+console.log(examPassed);
+/**
+output
+true
+**/
