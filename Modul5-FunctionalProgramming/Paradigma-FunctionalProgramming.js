d561e5c6d0709fcdbadcefdc383c81e788eb6158
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-13 18:48:01 +0700

    Added syntax "Gaya Deklaratif"

--- Modul5-FunctionalProgramming/Paradigma-FunctionalProgramming.js
+++ Modul5-FunctionalProgramming/Paradigma-FunctionalProgramming.js
@@ -2,7 +2,7 @@
 Functional Programming (selanjutnya akan kita singkat menjadi FP) ditulis dengan gaya deklaratif yang berfokus pada “what to solve” dibanding “how to solve” yang dianut oleh gaya imperatif.
 
 Sebagai gambaran buat Anda yang belum tahu apa itu deklaratif dan imperatif lebih jauh, silakan simak contoh kode berikut. */
-
+//* Gaya Imperatif
 const names = ['Harry', 'Ron', 'Jeff', 'Thomas'];
 
 const newNamesWithExcMark = [];
@@ -14,4 +14,15 @@ for(let i = 0; i< names.length; i++) {
 console.log(newNamesWithExcMark);
 /* output:
    [ 'Harry!', 'Ron!', 'Jeff!', 'Thomas!' ]
+*/
+
+//* Gaya Deklaratif
+const nama2 = ['Hafif', 'Ifal', "Alfi", 'Budi'];
+
+const newNama2WithExcMark = nama2.map((nama) => `${nama}!`);
+
+console.log(newNama2WithExcMark);
+
+/* output:
+*  [ 'Hafif!, 'Ifal!', 'Alfi!', 'Budi!' ]
 */
