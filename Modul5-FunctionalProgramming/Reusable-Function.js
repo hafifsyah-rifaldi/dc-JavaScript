7b253b92c6178c99d56f74990ad0277f5bf9af79
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-13 19:56:35 +0700

    Added code Array Filter

--- Modul5-FunctionalProgramming/Reusable-Function.js
+++ Modul5-FunctionalProgramming/Reusable-Function.js
@@ -15,3 +15,30 @@ console.log(truthyArray);
  * [ 1, 'Hallo', 'Harry', 14 ]
  * 
  */
+
+//* Contoh lain ARRAY FILTER
+const students = [
+    {
+        name: 'Harry',
+        score: 60,
+    },
+    {
+        name: 'James',
+        score: 88,
+    },
+    {
+        name: 'Ron',
+        score: 90,
+    },
+    {
+        name: 'Bethy',
+        score: 75,
+    }
+];
+const eligibleForScholarshipStudents = students.filter((student) => student.score > 85);
+console.log(eligibleForScholarshipStudents);
+/**
+ * output:
+ * [ { name: 'James', score: 88 }, { name: 'Ron', score: 90 } ]
+ * 
+ */
