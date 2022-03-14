dced960af401c88e81e6c051a04524453b356b69
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-14 14:55:42 +0700

    Edited Try Catch code

--- Modul8-PenangananError/TryCatch.js
+++ Modul8-PenangananError/TryCatch.js
@@ -3,10 +3,18 @@ try {
     errorCode;                      // (2)
     console.log("Akhir blok try"); // (3)
 } catch (error) {
-    console.log("Terjadi error!"); // (4)
+    console.log(error.name); // (4)
+    console.log(error.message);
+    console.log(error.stack);
 }
 
 /* output
 Awal blok try
-Terjadi error!
+ReferenceError
+errorCode is not defined
+ReferenceError: errorCode is not defined
+    at file:///home/dicoding/Playground/javascript/CoffeeMachine/error.js:3:5
+    at ModuleJob.run (internal/modules/esm/module_job.js:152:23)
+    at async Loader.import (internal/modules/esm/loader.js:166:24)
+    at async Object.loadESM (internal/process/esm_loader.js:68:5)
 */
