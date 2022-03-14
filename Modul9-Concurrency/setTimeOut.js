738990164a6e46bd00cafb9ac500a6a16be820de
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-14 20:26:10 +0700

    Added code setTimeout

---
+++ Modul9-Concurrency/setTimeOut.js
@@ -0,0 +1,10 @@
+/* Fungsi setTimeout() merupakan cara yang paling mudah untuk membuat kode kita dijalankan secara asynchronous. 
+Fungsi ini menerima dua buah parameter. Parameter pertama adalah fungsi yang akan dijalankan secara asynchronous. 
+Kedua adalah nilai number dalam milisecond sebagai nilai tunggu sebelum fungsi dijalankan. 
+Contoh penggunaannya adalah seperti ini: */
+
+console.log("Selamat Datang!");
+setTimeout(() => {
+    console.log("Terima kasih sudah mampir, silakan datang kembali!");
+}, 3000);
+console.log("Ada yang bisa dibantu?");
