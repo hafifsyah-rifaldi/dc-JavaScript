1bc38d9d2a25f55016e72024bbc4247c32cb6698
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-14 16:50:56 +0700

    KuisCoding-PenangananError

--- Modul8-PenangananError/KuisCoding-PenangananError.js
+++ Modul8-PenangananError/KuisCoding-PenangananError.js
@@ -42,35 +42,47 @@
  *   - bila block catch terpanggil, kembalikan fungsi detectTriangle dengan pesan error yang dibawa fungsi validateNumberInput.
  */
 
-const { min } = require("lodash");
-
 
 // TODO 1
 class ValidationError extends Error {
-    constructor(message){
-        super(message);
-        this.name = "Validation Error";
-    }
-}
+  constructor(message){
+      super(message);
+      this.name = "ValidationError";
+  }
+};
+
 // TODO 2
-validateNumberInput
+function validateNumberInput(a,b,c) {
+if (typeof(a) !=`number`){
+  throw new ValidationError('Argumen pertama harus number');
+}
+else if (typeof(b) != `number`){
+  throw new ValidationError('Argumen kedua harus number');
+}
+else if (typeof(c) != `number`){
+  throw new ValidationError('Argumen ketiga harus number');
+}
+}
 
 const detectTriangle = (a, b, c) => {
-    // TODO 3
-  
-    if (a === b && b === c) {
-      return 'Segitiga sama sisi';
-    }
-  
-    if (a === b || a === c || b === c) {
-      return 'Segitiga sama kaki';
-    }
-  
-    return 'Segitiga sembarang';
-  };
-  
-  /**
-   * Jangan hapus kode di bawah ini
-   */
-  module.exports = { ValidationError, validateNumberInput, detectTriangle };
-  
+  // TODO 3
+  try {
+    validateNumberInput(a,b,c); // (1)
+  } catch (error) { // (2)
+  return error.message; // (3)
+}
+  if (a === b && b === c) {
+    return 'Segitiga sama sisi';
+  }
+
+  if (a === b || a === c || b === c) {
+    return 'Segitiga sama kaki';
+  }
+
+  return 'Segitiga sembarang';
+};
+
+/**
+ * Jangan hapus kode di bawah ini
+ */
+module.exports = { ValidationError, validateNumberInput, detectTriangle };
