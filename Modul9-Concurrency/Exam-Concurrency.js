67e7fbe45e1e865c2423e72d6886fc5d85cefce8
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-15 11:49:57 +0700

    Added code Exam-Concurrency

--- Modul9-Concurrency/Exam-Concurrency.js
+++ Modul9-Concurrency/Exam-Concurrency.js
@@ -13,3 +13,16 @@ fetchUsername().then((value) => {
 .finally(() => {
     console.log("Welcome!");
 })
+
+function fetchUsername() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('JSUser');
+        }, 3000);
+    })
+}
+
+console.log("Fetching username...");
+const username = fetchUsername();
+console.log(`You are logged in as ${username}`);
+console.log("Welcome!");
