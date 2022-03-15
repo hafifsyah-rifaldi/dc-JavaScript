1f474cdd61a9de267e7c73478945917ec24f7bc3
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-15 21:50:55 +0700

    Update Repo

--- Modul11-JSTesting/averageExams.js
+++
@@ -1,6 +0,0 @@
-const {averageExams} = require('../gradeCalculations');
- 
-test('it should return exact average', () => {
-    const listValueOfExams = [80, 100, 100, 80];
-    expect(averageExams(listValueOfExams)).toEqual(90);
-})
--- Modul11-JSTesting/index.js
+++
@@ -1,9 +0,0 @@
-/* describe('pengujian olah aritmatika', () => {
-    test('#1 dua tambah dua adalah empat', () => {
-         expect(2+2).toBe(4);
-    });
-  
-    test('#2 dua kali tiga adalah enam', () => {
-         expect(2*3).toBe(6);
-    });
- }); */
