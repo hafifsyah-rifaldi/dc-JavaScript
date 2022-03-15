10c4d22bf36e88f350ccfb52dc01b713fa449ecb
hafifsyah-rifaldi <hafifsyahr@gmail.com>
2022-03-15 12:06:14 +0700

    Added reduce myArray

--- Modul10-NPM/UsingPackage.js
+++ Modul10-NPM/UsingPackage.js
@@ -1,11 +1,9 @@
 import _ from 'lodash';
 
 const myArray = [1, 2, 3, 4];
-let sum = 0;
-
-for(let i = 0; i < myArray.length; i++) {
-    sum += myArray[i];
-}
+let sum = myArray.reduce((prev, curr) => {
+    return prev + curr;
+});
 
 console.log(sum);
 
